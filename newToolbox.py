c750649533e1973107f925357b26a61073970295
justMega <maks.jagodic@gmail.com>
2024-12-13 17:43:29 +0100

    new

---
+++ newToolbox.py
@@ -0,0 +1,166 @@
+from bigtree import BinaryNode, print_tree
+import random
+
+class CueNode(BinaryNode):
+    def __init__(self, is_inverted, situation_index, left=None, right=None, parent=None):
+        super().__init__(left= left, right= right, parent= parent)
+        self.is_inverted = is_inverted
+        self.situation_index = situation_index
+        self.name = ("!" if self.is_inverted else "")+"c"+str(self.situation_index)
+
+    def decide(self, situation):
+        if situation[self.situation_index] != self.is_inverted:
+            return self.left
+        return self.right
+
+class ActionNode(BinaryNode):
+    def __init__(self, action, left=None, right=None, parent= None):
+        super().__init__(parent=parent, left=left, right=right)
+        self.action = action
+        self.name = "a"+str(self.action)
+
+class Tree():
+    def __init__(self, depth, actions, situation_amount) -> None:
+        self.root = CueNode(
+            random.choice([True, False]), 
+            random.randint(0, situation_amount-1))
+        tmp = CueNode(
+            random.choice([True, False]), 
+            random.randint(0, situation_amount-1),
+            parent= self.root)
+        self.actions = actions
+        self.situation_amount = situation_amount
+        self.build_tree(tmp, depth)
+
+    def build_tree(self, node, depth):
+        if depth <= 0:
+            ActionNode(parent= node, action= random.choice(self.actions))
+            ActionNode(parent= node, action= random.choice(self.actions))
+            return
+        
+        if random.uniform(0, 1) <= 0.5:
+            CueNode(    
+                random.choice([True, False]), 
+                random.randint(0, self.situation_amount-1),
+                parent= node)
+            self.build_tree(node.left, depth - 1)
+        else:
+            ActionNode(parent= node, action= random.choice(self.actions))
+
+        ActionNode(parent= node, action= random.choice(self.actions))
+
+    def get_action(self, situation):    
+        def get_action_rec(situation, node):
+            if isinstance(node, ActionNode):
+                return node.action
+            newNode = node.decide(situation)
+            if newNode is None:
+                if isinstance(node.left, ActionNode):
+                    return node.left.action
+                return node.left.decide(situation)
+            return get_action_rec(situation, newNode)
+        return get_action_rec(situation, self.root)
+    
+    def random_traverse(self):
+        current = self.root.left
+        take_next_step = random.uniform(0, 1)
+        while take_next_step <= 0.6:
+            if isinstance(current.left, ActionNode) or isinstance(current, ActionNode):
+                break
+            current = current.left
+            take_next_step = random.uniform(0, 1)
+        return current
+
+
+    def swap_nodes(self, n1, n2):
+        if n1 == n2:
+            return
+        if n1.depth < n2.depth:
+            n1, n2 = n2, n1
+
+        l1, r1 = n1.left, n1.right
+        l2, r2 = n2.left, n2.right
+        p1 = n1.parent
+        p2 = n2.parent
+
+        n1.left = None
+        n1.right = None
+        n2.left = None
+        n2.right = None
+        n1.parent = None
+        n2.parent = None
+
+        if p1 == n2:
+            n2.parent = n1
+        else:
+            n2.parent = p1
+        if p2 == n1:
+            n1.parent = n2
+        else:
+            n1.parent = p2
+
+        n2.right = r1
+        n1.right = r2
+
+        if l1 == n2:
+            n2.left = n1
+        else:
+            n2.left = l1
+        
+        if l2 == n1:
+            n1.left = n2
+        else:
+            n1.left = l2
+     
+    def delete_node(self, node):
+        p = node.parent
+        l = node.left
+        p.left = None
+        l.parent = None
+        p.left = l
+        l.parent = p
+        return l
+
+    def add_cue_action_pair(self, n1):
+        new_cue_node = CueNode(
+            random.choice([True, False]), 
+            random.randint(0, self.situation_amount-1))
+        n2 = n1.left
+        n1.left = None
+        new_cue_node.parent = n1
+        new_cue_node.left = n2
+        n2.parent = new_cue_node
+        n1.left = new_cue_node
+
+        ActionNode(
+            parent= new_cue_node, 
+            action= random.choice(self.actions))
+        return new_cue_node.left
+
+class ToolBox:
+    def __init__(self, depth, actions, situation_amount):
+        self.trees = []
+        for _ in range(5):
+            self.trees.append(Tree(depth= depth, actions= actions, situation_amount= situation_amount))
+
+    def show(self):
+        for tree in self.trees:
+            tree.show()
+
+    def get_action(self, situation):
+        for tree in self.trees:
+            if tree.root.decide(situation) is not None:
+                return tree.get_action(situation)
+        return self.trees[0].get_action(situation)
+
+if __name__ == "__main__":
+    print("lol")
+    t1 = Tree(depth= 3, actions=range(0,10), situation_amount= 10)
+    t1.root.show()
+    t1.swap_nodes(t1.root.left.left, t1.root.left)
+    t1.root.show()
+    t1.delete_node(t1.root.left.left)
+    t1.root.show()
+    t1.add_cue_action_pair(t1.root.left)
+    t1.root.show()
+
---
+++ newmain.py
@@ -0,0 +1,210 @@
+import random
+from newToolbox import ToolBox, Tree, ActionNode, CueNode
+import matplotlib.pyplot as plt
+import time
+
+def crossover(parent1, parent2):
+    p1 = random.uniform(0, 1)
+    if p1 <= 0.1:
+        p2 = random.uniform(0, 1)
+        if p2 <= 0.5:
+            c1 = random.randint(0, len(parent1.trees)-1)
+            c2 = random.randint(0, len(parent2.trees)-1)
+            parent1.trees = parent1.trees[:c1] + parent2.trees[c2:]
+            return parent1
+        else:
+            for h1, h2 in zip(parent1.trees, parent2.trees): # do we implement this get rid of all prior/all following ones?
+                node1 = h1.random_traverse()
+                node2 = h2.random_traverse()
+                if isinstance(node1, ActionNode) or isinstance(node2, ActionNode):
+                    continue
+                # basicaly when splicing two trees togheter we can create a loop
+                # so we check for that and only splice if shure we dont create a loop
+                current = node1
+                while not isinstance(current, ActionNode):
+                    if current == node2:
+                        break
+                    current = current.left
+                else:
+                    continue
+
+                node1.left = node2
+                node2.parent = node1
+
+                temp_left = node1.left
+                node1.left = node2.left
+                node2.left = temp_left
+
+                # Update the parent pointers for the swapped children
+                if node1.left is not None:
+                    node1.left.parent = node1
+                if node2.left is not None:
+                    node2.left.parent = node2
+                
+                # Swap the parent links of the nodes as well
+                temp_parent = node1.parent
+                node1.parent = node2.parent
+                node2.parent = temp_parent
+                
+                # Update the parent references after swapping
+                if node1.parent is not None:
+                    node1.parent.left = node1
+                if node2.parent is not None:
+                    node2.parent.left = node2
+                h1.show()
+                h2.show()
+            return parent1
+    else:
+        return parent1
+    
+def mutate(child):
+    copyList = child.trees.copy()
+    for i in range(len(child.trees)):
+        if len(copyList) < len(child.trees):
+            break
+        p1 = random.uniform(0,1)
+        if p1 <= 0.11:
+            p2 = random.uniform(0,1)
+            if p2 < 0.25:
+                # Change selector cue 
+                n = random.randint(0, SITUATION_AMOUNT-1)
+                copyList[i].situation_index = n
+            elif p2 < 0.50:
+                # insert new selector and heuristic
+                copyList.insert(i+1, Tree(DEPTH, ACTIONS, SITUATION_AMOUNT))
+            elif p2 < 0.75:
+                # Delete selector cue
+                if len(copyList) > 1:
+                    del copyList[i]
+            else:
+                # swap two selector cues
+                j = random.randint(0,len(child.trees)-1)
+                tmp = copyList[i]
+                copyList[i] = copyList[j]
+                copyList[j] = tmp
+    child.trees = copyList
+
+    for i, heuristic in enumerate(child.trees):
+        print("heuristic", i)
+        current = heuristic.root.left
+        while not isinstance(current, ActionNode):
+            #heuristic.show()
+            #time.sleep(0.01)
+            if current is None:
+                break
+            p1 = random.uniform(0,1)
+            if p1 <=0.11:
+                p2 = random.uniform(0,1)
+                if p2 <= 0.2:
+                    print("change cue")
+                    # change the curent situation index
+                    n = random.randint(0, SITUATION_AMOUNT-1)
+                    current.situation_index = n
+                elif p2 <= 0.4: 
+                    # change an action
+                    print("change action")
+                    n = random.choice(ACTIONS)
+                    current.right.action = n
+                elif p2 <= 0.6:
+                    print("add cue")
+                    current = heuristic.add_cue_action_pair(current)
+                elif p2 <= 0.8:
+                    print("delete cue")
+                    # delete the current node
+                    current = heuristic.delete_node(current)
+                else:
+                    print("swap cue")
+                    # swap curent node with a random node
+                    other = heuristic.random_traverse()
+                    print(str(other), str(current))
+                    if not isinstance(other, ActionNode) and other != current:
+                        heuristic.swap_nodes(current, other)
+                    current = other
+            current = current.left
+    return child
+    
+
+def get_incorrect_num(toolbox,world,situations):
+    score = 0
+    for situation in situations:
+        if toolbox.get_action(situation) == world.get_action(situation):
+            score +=1
+    return len(situations) - score # number of incorrect actions
+
+def fitness(toolbox,world,situations):
+    score = 0
+    for situation in situations:
+        if toolbox.get_action(situation) == world.get_action(situation):
+            score +=1
+    return score/len(situations) # number of incorrect actions
+
+# DEFINE GLOBAL CONSTANTS
+p = 0.0004 
+DEPTH = 5
+ACTIONS = range(1,50)
+SITUATION_AMOUNT = 10
+
+def run_simulation(depth = DEPTH, actions = ACTIONS, situation_amount = SITUATION_AMOUNT):
+    populations = []
+    avg_fitness = []
+    # Startt the main evolution algorithm
+    world = ToolBox(depth= depth, actions= actions, situation_amount= situation_amount)    
+    population = [ToolBox(depth= depth, actions= actions, situation_amount= situation_amount) for _ in range(500)]
+    count = 0
+    while 0 < len(population) < 600:
+        print("iteration: ", count)
+        count += 1
+        situations = [[random.choice([True,False]) for _ in range(SITUATION_AMOUNT)] for _ in range(2**SITUATION_AMOUNT)]
+        fitnesses = []
+        popcopy = population.copy()
+        for i, individual in enumerate(population): # remove underperforming individuals
+            print("induvidual: ", i)
+            num_incorrect = get_incorrect_num(individual, world, situations) 
+            s = (1 - p)**num_incorrect # 1 - p is chance of survival, with more incorrect actions, more likely to die
+            f = fitness(individual, world, situations)
+            fitnesses.append(f)
+            k = random.uniform(0, 1) 
+            if k > s: # the smaller s, the more likely k is bigger than s, so the more likely to be removed
+                popcopy.remove(individual)
+        population = popcopy
+        print(fitnesses)
+        avg_fitness.append(sum(fitnesses)/len(fitnesses))
+        print('Killed unperformers')
+        newPopulation = []
+        for individual in population: # choose two parents, choose num of children, crossover & mutate heuristics for children
+            parent1 = individual
+            num_children = 1
+            k = random.uniform(0, 1)
+            if k <= 0.333: # 0.474 for supplementary materials 
+                num_children = 2
+            print(time.time(), 'It reaches this for loop')
+            for j in range(num_children):
+                #print("mutating: ", j)
+                parent2 = random.choice(population)
+                if parent2 == parent1:
+                    continue
+                #print(type(parent2), parent2)
+                child = crossover(parent1, parent2)
+                print("done crosover: ", type(child))
+                child = mutate(child)
+                print("done mutating: ", type(child))
+                newPopulation.append(child)
+        population = newPopulation
+        populations.append(len(population)) 
+    return populations, avg_fitness
+
+def main():
+    data, fitnesses = run_simulation()
+    print(len(data), data)
+    plt.plot(data)
+    plt.xlabel('Generation')
+    plt.ylabel('Population')
+    plt.show()
+
+    plt.plot(fitnesses)
+    plt.xlabel('Generation')
+    plt.ylabel('avg fitness')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
